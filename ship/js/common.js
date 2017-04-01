d93628972784714831a6072204ac1f1141053a6c
ARTEM <voloskovskiy1996@gmail.com>
2017-04-01 13:54:19 +0300

    add new prod
    add new prod

---
+++ ship/js/common.js
@@ -0,0 +1,11 @@
+$(function() {
+
+$('.search').click(function(){
+	$('.search').addClass("none");
+	$('.search_pole').addClass("view");
+});
+$('.send').click(function(){
+	$(".first_display").addClass("none");
+	$('.message_end').addClass("view_flex");
+});
+});
